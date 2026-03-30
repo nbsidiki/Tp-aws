957c1cd86a010b25038537ae3910b6fabd9770e8
aboubacar <sidikinabe10@gmail.com>
2026-03-30 17:11:11 +0200

    Add outputs for database EC2 instance ID and public IP in Terraform configuration

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,13 @@ output "bucket_id" {
   description = "ID of the S3 bucket"
   value       = aws_s3_bucket.demo_bucket.id
 }
+
+output "db_instance_id" {
+  description = "ID of the database EC2 instance"
+  value       = aws_instance.db.id
+}
+
+output "db_instance_public_ip" {
+  description = "Public IP of the database EC2 instance"
+  value       = aws_instance.db.public_ip
+}
